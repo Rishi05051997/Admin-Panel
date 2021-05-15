d747cd9e89b38cfaf5881321612a74cf7ac876b3
vrushabhdhatrak <vrushabhdhatral10@gmail.com>
2021-05-15 15:54:17 +0530

    Project done

---
+++ front-end/admin-panel/src/app/dashboard/layout/side-nav/side-nav.component.ts
@@ -0,0 +1,45 @@
+import { Component, NgZone, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.scss']
+})
+export class SideNavComponent implements OnInit {
+
+  private mediaMatcher: MediaQueryList
+  = window.matchMedia('(max-width: 720px)');
+
+  links = [
+    {
+      name: 'Hr-Managemennt',
+      url: 'hr'
+    },
+    // {
+    //   name: 'Clients',
+    //   url: 'clients'
+    // },
+  ]
+
+  constructor(zone: NgZone) {
+    let media = this.mediaMatcher;
+    media.addEventListener('change',(mql:any) => {
+      // console.log(mql);
+      zone.run(() => media = mql);
+
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  isScreenSmall() {
+    return this.mediaMatcher.matches;
+  }
+
+
+
+
+}
---
+++ front-end/admin-panel/src/app/dashboard/users/models/users-model.ts
@@ -0,0 +1,12 @@
+
+export interface Users {
+    _id: String,
+    empId: string,
+    name: String,
+    email: String,
+    status: String | Boolean,
+    location: String,
+    role: String,
+    password: String,
+  }
+
